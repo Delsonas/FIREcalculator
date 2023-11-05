d87c3745ec6eaebb4dcd4b3ad6da695bcbfe75af
Vita <1>
2023-11-05 10:31:11 +0500

    Тестовое для Толи

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,7 @@
-
 public class Main {
-
     public static void main(String[] args) throws Exception {
-
         int input = Start.start();
-
         FireCalculator fireCalculator = new FireCalculator();
-
         System.out.println(fireCalculator.calculate(input));
     }
 }
--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -3,21 +3,17 @@ import java.util.Scanner;
 public class Start {
     public static int start() throws Exception{
         System.out.println("Привет! Давай рассчитаем максимальный процент изъятия.");
-
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Введи год, начиная с которого ты будешь жить на проценты(2002-2022) ");
         String inputLine = "";
-
         inputLine = keyboard.nextLine();
         keyboard.close();
 
         //проверка, что ввели число, а не текст, нужна?
         int input = Integer.parseInt(inputLine);
-
         if (input > 2022 || input < 2002) {
             throw new Exception("Ввели не подходящий год");
         }
-
         return input;
     }
 }
