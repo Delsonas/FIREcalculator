a1b21098da4fc4a6e9c36a57d511ebc78da7f101
Vita <1>
2023-11-06 14:34:16 +0500

    Тестовое для Толи

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -4,14 +4,14 @@ public class Start {
     public static int start() throws Exception{
         System.out.println("Привет! Давай рассчитаем максимальный процент изъятия.");
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Введи год, начиная с которого ты будешь жить на проценты(2002-2022) ");
+        System.out.println("Введи год, начиная с которого ты будешь жить на проценты(2002-2021) ");
         String inputLine = "";
         inputLine = keyboard.nextLine();
         keyboard.close();
 
         //проверка, что ввели число, а не текст, нужна?
         int input = Integer.parseInt(inputLine);
-        if (input > 2022 || input < 2002) {
+        if (input > 2021 || input < 2002) {
             throw new Exception("Ввели не подходящий год");
         }
         return input;
