725ff358d891d76684ba3eb88a146b8964ac5647
Vita <1>
2023-11-07 13:54:36 +0500

    Тестовое для Толи

--- src/main/java/FireCalculator.java
+++ src/main/java/FireCalculator.java
@@ -1,22 +1,24 @@
 public class FireCalculator {
     public double calculate(int input) {
-        int date = 0;
+        int date;
         double percent = 0.5;
-        for (double testSum = 100; testSum > 0; ) {
+        for (double testSum = 100; testSum > 0; percent += 0.5) {
             testSum = 100;
             for (date = input - 2002; date < 20; date++) {
                 testSum -= percent;
                 //если 2002 год, то мы не считаем увеличение/уменьшение капитала в этот год, т.к. нет данных из предыдущего года
-                if (date != 0){
-                    testSum = testSum +
-                            testSum*(((Constants.MOEX_RATE[date] - Constants.MOEX_RATE[date - 1]) / Constants.MOEX_RATE[date - 1] * 100)
-                                    -  Constants.INFLATION_RATE[date])/100;
+                if (date > 0) {
+                    testSum = testSum *
+                            (1 + (((Constants.MOEX_RATE[date] - Constants.MOEX_RATE[date - 1]) / Constants.MOEX_RATE[date - 1])
+                                    - Constants.INFLATION_RATE[date] / 100));
                 }
             }
-            percent += 0.5;
+            testSum -= percent; /* когда 2022 наступал, мы выходили из цикла внутреннего и не делали вычитание процента изъятия
+             (чтобы не умереть с голоду в 22 году)*/
         }
         return percent;
     }
 }
+
 
 
