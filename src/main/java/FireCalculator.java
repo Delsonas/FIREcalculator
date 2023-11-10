76ad9fa77b278bada610245eaaa690b67301c1b7
Vita <1>
2023-11-10 12:41:19 +0500

    Тестовое для Толи

--- src/main/java/FireCalculator.java
+++ src/main/java/FireCalculator.java
@@ -1,23 +1,23 @@
 public class FireCalculator {
-    private static final int START_SUM = 100;
+    private static final int START_SUM = 1;
     private static final int START_DATE = 2002;
     private static final int END_DATE = 2022;
-    private static final double PERCENT_START = 0.5;
+    private static final double PERCENT_START = 0;
     private static final double PERCENT_DELTA = 0.5;
+
     public double calculate(int input) {
         double percentMax;
         for (double percent = PERCENT_START; ; percent += PERCENT_DELTA) {
             double testSum = findTestSumForPercent(input, percent);
             percentMax = percent;
-            if (testSum < 0) {
+            if (testSum <= 0) {
                 break;
             }
         }
         return percentMax;
     }
-
     private double findTestSumForPercent(int input, double percent) {
-        double testSum = START_SUM;
+        double testSum = 0;
         double percentAccumulated = percent;
         for (int date = input - START_DATE; date < END_DATE - START_DATE; date++) {
             //если 2002 год, то мы не считаем увеличение/уменьшение капитала и в этот год и инфляцию за прошлый, т.к. нет данных из предыдущего года
@@ -25,11 +25,11 @@ public class FireCalculator {
                 testSum -= percentAccumulated;
                 continue;
             }
-            percentAccumulated = (percentAccumulated * (1 + Constants.INFLATION_RATE[date - 1] / 100));
             testSum -= percentAccumulated;
-            testSum = testSum *
-                    (1 + (Constants.MOEX_RATE[date] - Constants.MOEX_RATE[date - 1]) / Constants.MOEX_RATE[date - 1]);
-        }
+            testSum += ((Constants.MOEX_RATE[date] - Constants.MOEX_RATE[date - 1]) / Constants.MOEX_RATE[date - 1])*100 - Constants.INFLATION_RATE[date - 1];
+            percentAccumulated = (percentAccumulated * (1 + Constants.INFLATION_RATE[date - 1] / 100));
+
+       }
         testSum -= percentAccumulated; /* когда 2022 наступал, мы выходили из цикла внутреннего и не делали вычитание процента изъятия
          (чтобы не умереть с голоду в 22 году)*/
         return testSum;
